122a86cb55c33804833f6249cd5da22f8285baf7
Weobe <duruozer13@gmail.com>
2025-07-07 17:40:11 -0700

    Fixed bug related to path dependency

--- main/send_pod_kudos.sh
+++ main/send_pod_kudos.sh
@@ -2,5 +2,13 @@
 
 # Exit on error
 set -e
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do                       # $SOURCE is a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"                 # follow the link one step
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"   # handle relative links
+done
+SCRIPT_DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
 
+cd "$SCRIPT_DIR"
 cargo run --release -- "$@"
